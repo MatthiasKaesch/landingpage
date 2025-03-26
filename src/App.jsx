1c3bf1fdbb8eb42571e3634ba0cd3e9b6d1fd032
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-03-26 13:36:26 +0100

    add basic layout and header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import './App.css'
 import './styling/normalize.css'
+import Thumbnail from './assets/images/event-table.webp'
 
 function App() {
   return (
-    <div className="wrapper">
-      <h1>Portfolio</h1>
-      <div class="projectsGrid">
-        <a href="https://matthiaskaesch.github.io/event-table/">
-          <iframe
-            width="300"
-            height="200"
-            src="https://matthiaskaesch.github.io/event-table/"
-            frameborder="0"
-          ></iframe>
-        </a>
+    <>
+      <header className="header">
+        <h1>Matthias Käsch - Frontend Developer</h1>
+      </header>
+
+      <div className="wrapper">
+        <div className="projects-grid">
+          <div className="project-1">
+            <a href="https://matthiaskaesch.github.io/event-table/">
+              <img src={Thumbnail} alt="" />
+            </a>
+          </div>
+          <div className="project-2">
+            <a href="https://matthiaskaesch.github.io/event-table/">
+              <img src={Thumbnail} alt="" />
+            </a>
+          </div>
+          <div className="project-3">
+            <a href="https://matthiaskaesch.github.io/event-table/">
+              <img src={Thumbnail} alt="" />
+            </a>
+          </div>
+          <div className="project-4">
+            <a href="https://matthiaskaesch.github.io/event-table/">
+              <img src={Thumbnail} alt="" />
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
