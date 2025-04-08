e8f98bf116d454b5016238a7a6965705c3b97c2a
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-04-08 19:18:22 +0200

    add social bar

---
+++ src/components/SocialBar/SocialBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './SocialBar.css'
+import GitHubIcon from '../Icons/GitHubIcon'
+import LinkedInIcon from '../Icons/LinkedInIcon'
+import MailIcon from '../Icons/MailIcon'
+
+const SocialBar = () => {
+  return (
+    <div className="social-bar">
+      <a
+        href="https://github.com/MatthiasKaesch/landingpage"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GitHubIcon />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/matthias-kaesch/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <LinkedInIcon />
+      </a>
+      <a href="mailto:matthias.kaesch@gmail.com">
+        <MailIcon />
+      </a>
+    </div>
+  )
+}
+
+export default SocialBar
