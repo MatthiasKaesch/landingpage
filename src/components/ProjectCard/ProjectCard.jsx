73656b9a1c761ccdda01623cc673cfeb4e0ef839
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-04-08 19:18:38 +0200

    update link in projectcard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -16,7 +16,12 @@ const ProjectCard = ({ title, projectUrl, thumbnail, techStack, colors }) => {
       }}
     >
       <h2 className="portal-title">{title}</h2>
-      <a className="portal-link" href={projectUrl} target="_blank">
+      <a
+        className="portal-link"
+        href={projectUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <WarpedBackground src={`/landingpage/assets/images/${thumbnail}`} />
       </a>
     </div>
