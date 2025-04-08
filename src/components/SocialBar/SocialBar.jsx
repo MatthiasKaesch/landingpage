3285f062dced8015b459030fcbaebda8009245b9
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-04-08 20:10:14 +0200

    updated social bar

--- src/components/SocialBar/SocialBar.jsx
+++ src/components/SocialBar/SocialBar.jsx
@@ -6,24 +6,26 @@ import MailIcon from '../Icons/MailIcon'
 
 const SocialBar = () => {
   return (
-    <div className="social-bar">
-      <a
-        href="https://github.com/MatthiasKaesch/landingpage"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <GitHubIcon />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/matthias-kaesch/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <LinkedInIcon />
-      </a>
-      <a href="mailto:matthias.kaesch@gmail.com">
-        <MailIcon />
-      </a>
+    <div className="social-bar-container">
+      <div className="social-bar">
+        <a
+          href="https://github.com/MatthiasKaesch/landingpage"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/matthias-kaesch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
+        <a href="mailto:matthias.kaesch@gmail.com">
+          <MailIcon />
+        </a>
+      </div>
     </div>
   )
 }
