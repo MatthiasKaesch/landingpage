532aa156fe92e59b3d55f6a4ede946bf5986dbef
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-04-08 19:18:03 +0200

    add social icons

---
+++ src/components/Icons/MailIcon.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MailIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M20 4H4c-1.11 0-2 .89-2 2v12c0 1.104.89 2 2 2h16c1.104 0 2-.896 2-2V6c0-1.11-.896-2-2-2zm0 2v.511l-8 5.333-8-5.333V6h16zM4 18V8.489l8 5.333 8-5.333V18H4z" />
+  </svg>
+)
+
+export default MailIcon
