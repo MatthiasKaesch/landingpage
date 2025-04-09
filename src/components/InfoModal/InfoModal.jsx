17e219de569b451b19d921a04ce81958028ef2f3
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-04-09 15:36:31 +0200

    use new hooks in modal

--- src/components/InfoModal/InfoModal.jsx
+++ src/components/InfoModal/InfoModal.jsx
@@ -1,52 +1,14 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import { useFocusRestore } from '../../hooks/useFocusRestore'
+import { useFocusTrap } from '../../hooks/useFocusTrap'
+import { useEscapeKey } from '../../hooks/useEscapeKey'
 import './InfoModal.css'
 
 const InfoModal = ({ isOpen, onClose, title, techStack, desc }) => {
   const dialogRef = useRef()
+  useFocusTrap(dialogRef, isOpen)
   useFocusRestore(isOpen)
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === 'Escape') {
-        onClose()
-      }
-    }
-
-    if (isOpen) {
-      document.addEventListener('keydown', handleKeyDown)
-      dialogRef.current?.focus()
-    }
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [isOpen, onClose])
-
-  const handleTab = (e) => {
-    const focusable = dialogRef.current.querySelectorAll(
-      'button, [href], input, [tabindex]:not([tabindex="-1"])',
-    )
-    const first = focusable[0]
-    const last = focusable[focusable.length - 1]
-
-    if (e.key === 'Tab') {
-      if (e.shiftKey && document.activeElement === first) {
-        e.preventDefault()
-        last.focus()
-      } else if (!e.shiftKey && document.activeElement === last) {
-        e.preventDefault()
-        first.focus()
-      }
-    }
-  }
-
-  useEffect(() => {
-    const lastFocused = document.activeElement
-    return () => {
-      lastFocused?.focus()
-    }
-  }, [])
+  useEscapeKey(onClose, isOpen)
 
   if (!isOpen) return null
 
@@ -59,7 +21,6 @@ const InfoModal = ({ isOpen, onClose, title, techStack, desc }) => {
         aria-labelledby="modal-title"
         tabIndex="-1"
         ref={dialogRef}
-        onKeyDown={handleTab}
       >
         <div className="modal-header">
           <h3 id="modal-title">{title}</h3>
